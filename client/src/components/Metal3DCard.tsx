f6e34183442853da87fc402671652c53ec613017
zjondreangermund <zjondreangermund@gmail.com>
2026-04-21 15:15:46 +0200

    Refactor Metal3DCard to wrapper over UnifiedPlayerCard

--- client/src/components/Metal3DCard.tsx
+++ client/src/components/Metal3DCard.tsx
@@ -1,4 +1,4 @@
-import SimpleCard from "./SimpleCard";
+import UnifiedPlayerCard from "./cards/UnifiedPlayerCard";
 import { type PlayerCardData } from "./cards/types";
 
 type Metal3DCardProps = {
@@ -6,10 +6,6 @@ type Metal3DCardProps = {
   className?: string;
 };
 
-/**
- * Compatibility wrapper:
- * We intentionally keep a single lightweight 2D rendering path in production.
- */
 export default function Metal3DCard({ player, className = "" }: Metal3DCardProps) {
-  return <SimpleCard player={player} className={className} />;
+  return <UnifiedPlayerCard player={player} className={className} />;
 }
