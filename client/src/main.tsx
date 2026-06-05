ecfdc87ca3fbbc8f12e395e153cc443655b7e478
zjondreangermund <zjondreangermund@gmail.com>
2026-06-05 07:36:50 +0200

    Import mobile polish styles

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import "./mobile-polish.css";
 import { patchFetchForApiBase } from "./lib/api-base";
 
 patchFetchForApiBase();
