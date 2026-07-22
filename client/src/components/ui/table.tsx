78ffe5cab3cf12e7554e153c223c60537b1d2a2c
zjondreangermund <zjondreangermund@gmail.com>
2026-07-22 16:01:00 +0200

    Limit table wrappers to horizontal scrolling

--- client/src/components/ui/table.tsx
+++ client/src/components/ui/table.tsx
@@ -6,7 +6,7 @@ const Table = React.forwardRef<
   HTMLTableElement,
   React.HTMLAttributes<HTMLTableElement>
 >(({ className, ...props }, ref) => (
-  <div className="relative w-full overflow-auto">
+  <div className="relative w-full min-w-0 overflow-x-auto overflow-y-clip" data-mobile-x-scroll="true">
     <table
       ref={ref}
       className={cn("w-full caption-bottom text-sm", className)}
