e9fd6912bf9159fcf355499ff5183d3b5cf5e598
zjondreangermund <zjondreangermund@gmail.com>
2026-09-09 10:26:34 +0200

    Point Android download to 1.1.8

--- client/src/components/InstallAppButton.tsx
+++ client/src/components/InstallAppButton.tsx
@@ -8,8 +8,8 @@ type BeforeInstallPromptEvent = Event & {
 };
 
 const ANDROID_APK_BASE_URL =
-  "https://github.com/zjondreangermund/Fantasy-Sports-Exchange/releases/download/android-1.1.7/Fantasy-Arena-Android.apk";
-const ANDROID_VERSION = "1.1.7";
+  "https://github.com/zjondreangermund/Fantasy-Sports-Exchange/releases/download/android-1.1.8/Fantasy-Arena-Android.apk";
+const ANDROID_VERSION = "1.1.8";
 const ANDROID_SIZE_LABEL = "5.7 MB";
 
 let deferredInstallPrompt: BeforeInstallPromptEvent | null = null;
