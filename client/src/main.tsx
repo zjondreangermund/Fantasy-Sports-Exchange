407e1a3fd5afc834250585cad5d693f7407b9671
zjondreangermund <zjondreangermund@gmail.com>
2026-09-13 07:30:54 +0200

    Load browser zoom and pan safeguards last

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -11,6 +11,7 @@ import "./onboarding-card-clipping-fix.css";
 import "./legal-tabs-slider.css";
 import "./native-mobile.css";
 import "./push-notification-visibility.css";
+import "./browser-zoom-pan.css";
 import { patchFetchForApiBase } from "./lib/api-base";
 import { initializeSiteView, isNativeMobileApp } from "./lib/site-view";
 import { captureGw4MetaHomepageVisit } from "./lib/gw4-promo";
