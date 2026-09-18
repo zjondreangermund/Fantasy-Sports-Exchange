c1cdc385193ed9bddfe697df23e99ceadecb2f3c
zjondreangermund <zjondreangermund@gmail.com>
2026-09-18 08:27:35 +0200

    Mount signup welcome popup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -25,6 +25,7 @@ import GuidedHoverHelp from "./components/GuidedHoverHelp";
 import GlobalActionToasts from "./components/GlobalActionToasts";
 import CommonCardRewardChoiceDialog from "./components/CommonCardRewardChoiceDialog";
 import PendingPrizeClaimPopup from "./components/PendingPrizeClaimPopup";
+import SignupWelcomePopup from "./components/SignupWelcomePopup";
 import { useAuth } from "./hooks/use-auth";
 import { useScrollRepair } from "./hooks/use-scroll-repair";
 import { applySiteView, getSiteViewMode, isNativeMobileApp, type SiteViewMode } from "./lib/site-view";
@@ -242,6 +243,7 @@ export default function App() {
           <SecurityModeBanner />
           {!nativeApp ? <GuidedHoverHelp /> : null}
           <AppContent />
+          <SignupWelcomePopup />
           <PendingPrizeClaimPopup />
           <CommonCardRewardChoiceDialog />
           <GlobalActionToasts />
